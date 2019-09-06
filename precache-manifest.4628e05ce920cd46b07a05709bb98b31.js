56123698716d546546f1101fd81ffd07307add9e
INDRIVETEC\f.conticelli <f.conticelli@indrivetec.ch>
2019-09-06 13:06:15 +0200

    Updates

---
+++ precache-manifest.4628e05ce920cd46b07a05709bb98b31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd423347df66eaccdeba",
+    "url": "/tutorial/static/js/runtime~main.cd423347.js"
+  },
+  {
+    "revision": "a5f6c6e5dcf308268c74",
+    "url": "/tutorial/static/js/main.a5f6c6e5.chunk.js"
+  },
+  {
+    "revision": "f3b84ad1a4a60d245526",
+    "url": "/tutorial/static/js/1.f3b84ad1.chunk.js"
+  },
+  {
+    "revision": "a5f6c6e5dcf308268c74",
+    "url": "/tutorial/static/css/main.8951fa1c.chunk.css"
+  },
+  {
+    "revision": "e556079a4ba3e5cdf9f8a1ac2fa1e629",
+    "url": "/tutorial/index.html"
+  }
+];
